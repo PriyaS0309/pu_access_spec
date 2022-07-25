ed45b331cfc5aedf1f74c0c7c0e8187c74809767
PriyaS0309 <109306451+PriyaS0309@users.noreply.github.com>
2022-07-25 16:51:55 +0530

    Add project files.

---
+++ pu_access1/Program1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pu_access1
+{
+    public class Sample
+    {
+        public void Example()
+        {
+            Console.WriteLine("This is the examplem of public access modifiers");
+        }
+
+    }
+
+   
+
+   
+
+  
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Sample s = new Sample();
+            s.Example(); 
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ pu_access2/Program2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using pu_access1;     //adding pu_access1 assembly
+
+namespace pu_access2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Sample s1 = new Sample();                     // using public methods of one assembly into another
+            s1.Example();
+
+            Console.ReadLine();
+        }
+    }
+}
